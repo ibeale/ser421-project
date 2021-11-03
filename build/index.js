d1f0725bb34f25ac3393eaa1885d4a0ad1c8bdfc
Isaac Beale <isaacdbeale@gmail.com>
2021-11-03 13:18:26 -0700

    good dev flow and started skeleton

--- build/index.js
+++ build/index.js
@@ -4,9 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var path_1 = __importDefault(require("path"));
 var app = (0, express_1.default)();
+app.set('view engine', 'pug');
+app.set('views', path_1.default.join(__dirname, 'views'));
+// app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function (req, res) {
-    res.send('Well done!');
+    res.render('home');
 });
 app.listen(3000, function () {
     console.log('The application is listening on port 3000!');
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import path from 'path';
+import _ from 'underscore'
 
 const app = express();
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.send('Well done!');
+    res.render('home')
 })
 
 app.listen(3000, () => {
