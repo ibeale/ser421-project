8851ab406cd6e19d0f6e56a1d47bd5be4055d1a6
Isaac Beale <isaacdbeale@gmail.com>
2021-11-03 20:18:35 -0700

    all pages made & remove build dir from repo

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,26 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/objectives', (req, res) => {
+    res.render('learningObjectives')
+})
+
+app.get('/tutorial', (req, res) => {
+    res.render('tutorial')
+})
+
+app.get('/history', (req, res) => {
+    res.render('history')
+})
+
+app.get('/analytical', (req, res) => {
+    res.render('analytical')
+})
+
+app.get('/demo', (req, res) => {
+    res.render('demo')
+})
+
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
 })
