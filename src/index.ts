7c1c4d41e48e6f48f535c7e1dbe69c30ff2d2051
Isaac Beale <isaacdbeale@gmail.com>
2021-11-14 21:17:26 -0700

    added some tutorial pages

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import _ from 'underscore'
 const app = express();
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('home')
---
+++ src/public/js/todo.ts
@@ -0,0 +1,89 @@
+interface todoItem{
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+interface todoList{
+    todos: todoItem[];
+    addTodo(text: string): void;
+    toggleTodo(id: number): EventListener;
+    clearCompleted: () => EventListener;
+    renderTodos(): void;
+}
+
+let todoList: todoList = {
+    todos: [],
+    addTodo(text: string){
+        this.todos.push({
+            id: this.todos.length,
+            text: text,
+            completed: false
+        });
+    },
+    toggleTodo(id: number){
+        let e: EventListener = (e: Event) => {
+            this.todos[id].completed = !this.todos[id].completed;
+            this.renderTodos();
+        }
+        return e;
+    },
+    clearCompleted(){
+        return (e: Event) => {
+            this.todos = this.todos.filter(todo => !todo.completed);
+            this.renderTodos();
+        }
+    },
+    renderTodos(){
+        let todoListElement = document.getElementById("todoList");
+        if(todoListElement){
+            while(todoListElement.firstChild){
+                todoListElement.removeChild(todoListElement.firstChild);
+            }
+            for(let todo of this.todos){
+                let todoItemElement = document.createElement("li");
+                todoItemElement.setAttribute("id", "todoItem" + todo.id);
+                
+                let todoCheckboxElement = document.createElement("input");
+                todoCheckboxElement.addEventListener('click', todoList.toggleTodo(todo.id));
+                todoCheckboxElement.setAttribute("type", "checkbox");
+                todoCheckboxElement.setAttribute("id", "todoCheckbox" + todo.id);
+                (todoCheckboxElement as HTMLInputElement).checked = todo.completed;
+                
+                let todoCheckboxElementLabel = document.createElement("label");
+                todoCheckboxElementLabel.setAttribute("for", "todoCheckbox" + todo.id);
+                todoCheckboxElementLabel.innerText = todo.text;
+                
+                todoItemElement.appendChild(todoCheckboxElement);
+                todoItemElement.appendChild(todoCheckboxElementLabel);
+                todoListElement.appendChild(todoItemElement);
+            }
+        }
+    }
+};
+
+function addTodoItemToList(){
+    let todoInputElement = document.getElementById("todoInput");
+    if(todoInputElement){
+        let todoText = (todoInputElement as HTMLInputElement).value;
+        if(todoText.length > 0){
+            todoList.addTodo(todoText);
+            (todoInputElement as HTMLInputElement).value = "";
+        }
+    }
+    todoList.renderTodos();
+    
+}
+
+window.onload = () => {
+    let addTodoButton: HTMLElement | null = document.getElementById('addTodo');
+    if(addTodoButton){
+        (addTodoButton as HTMLButtonElement).addEventListener('click', addTodoItemToList);
+    }
+
+    let clearCompletedButton: HTMLElement | null = document.getElementById('clearCompleted');
+    if(clearCompletedButton){
+        (clearCompletedButton as HTMLButtonElement).addEventListener('click', todoList.clearCompleted());
+    }
+    
+}
